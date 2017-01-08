218a44c0dfd0812c9a342c6d001838c01b1f1457
Nuckinfutz <amugno@charter.net>
2017-01-07 21:50:39 -0500

    Update play-video.py

--- play-video.py
+++ play-video.py
@@ -7,6 +7,7 @@ import os, random
 randomfile = random.choice(os.listdir("js/videos"))
 file = 'js/videos/'+ randomfile
 print("Going to play: " + file)
-os.system ('echo 0 > /sys/class/backlight/rpi_backlight/bl_power')
+os.system ('sudo chmod 777 /sys/class/backlight/rpi_backlight/bl_power')
+os.system ('sudo echo 0 > /sys/class/backlight/rpi_backlight/bl_power')
 os.system ('omxplayer --aspect-mode stretch ' + file)
-os.system ('echo 1 > /sys/class/backlight/rpi_backlight/bl_power')
+os.system ('sudo echo 1 > /sys/class/backlight/rpi_backlight/bl_power')
